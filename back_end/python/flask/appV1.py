514b9672389d97d135c984a6e2477d065e4f9e37
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2021-09-19 16:49:46 -0300

    API cadastro de DEV's
    Desenvolvimento de API para realização de cadastro de desenvolvadores.

---
+++ back_end/python/flask/appV1.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+import json
+
+
+app = Flask(__name__)
+
+devs = [
+    {
+        'name':'Wanderson Fontes',
+        'techStack': ['Python', 'PHP', 'JavaScript'],
+        'level': 'Full'
+    },
+    {
+        'name':'Thiago Navarro',
+        'techStack': ['Ruby', 'C', 'Delphi'],
+        'level': 'Full'
+    },
+]
+
+@app.route('/devs', methods=['GET', 'POST', 'DELETE'])
+def infoDevs():
+    if request.method == 'POST':
+        try:
+            devs.append(json.loads(request.data))
+        except Exception as e:
+            return e      
+    elif request.method == 'DELETE':
+        try:
+            devs.clear()
+        except Exception as e:
+            return e  
+    elif request.method == 'GET':
+        pass        
+    return jsonify(devs)
+
+@app.route('/devs/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def infoDevID(id):
+    if request.method == 'DELETE':
+        # devs.pop(id)
+        return jsonify({'status':'REMOVED SUCESSFULY'})
+    if request.method == 'GET': 
+        pass
+    elif request.method == 'PUT':
+        data = json.loads(request.data)
+        devs[id] = data
+    return jsonify(devs[id])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
